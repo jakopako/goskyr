fc6c49350f87bd0b55752c76f68910ad72a02367
jakopako <jakob.dhondt@web.de>
2022-12-04 15:45:58 +0100

    added 2 more tests

--- automate/config_test.go
+++ automate/config_test.go
@@ -1,6 +1,10 @@
 package automate
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/jakopako/goskyr/scraper"
+)
 
 func TestPathToSelector(t *testing.T) {
 	path := []string{"body.home", "div.wrapper.schedule", "div.event-list", "a.item"}
@@ -41,5 +45,47 @@ func TestNotNodesEqual(t *testing.T) {
 		if nodesEqual(n, n2[i]) {
 			t.Fatalf("nodes %s and %s are not equal", n, n2[i])
 		}
+	}
+}
+
+func TestFilter(t *testing.T) {
+	l := []*locationProps{
+		{
+			loc:   scraper.ElementLocation{},
+			count: 4,
+		},
+		{
+			loc:   scraper.ElementLocation{},
+			count: 8,
+		},
+	}
+	nl := filter(l, 8, false)
+	if len(nl) != 1 {
+		t.Fatalf("expected only 1 element to remain after filtering but got %+v", nl)
+	}
+	if nl[0].count != 8 {
+		t.Fatal("wrong element filtered.")
+	}
+}
+
+func TestNoStaticFieldsFilter(t *testing.T) {
+	l := []*locationProps{
+		{
+			loc:      scraper.ElementLocation{},
+			count:    4,
+			examples: []string{"a", "a"},
+		},
+		{
+			loc:      scraper.ElementLocation{},
+			count:    8,
+			examples: []string{"a", "b"},
+		},
+	}
+	nl := filter(l, 4, true)
+	if len(nl) != 1 {
+		t.Fatalf("expected only 1 element to remain after filtering but got %+v", nl)
+	}
+	if nl[0].count != 8 { // the remaining element should have count 8
+		t.Fatal("wrong element filtered.")
 	}
 }

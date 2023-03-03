686b9b55c94e16860ae5b93c82f99e250a8a88b5
jakopako <jakob.dhondt@web.de>
2023-03-03 17:11:11 +0100

    struggling with categorical classes

--- ml/ml.go
+++ ml/ml.go
@@ -16,8 +16,9 @@ import (
 )
 
 type Labler struct {
-	wordMap map[string]bool
-	cls     *knn.KNNClassifier
+	wordMap    map[string]bool
+	cls        *knn.KNNClassifier
+	labelsAttr base.Attribute
 }
 
 func LoadLabler() (*Labler, error) {
@@ -26,10 +27,22 @@ func LoadLabler() (*Labler, error) {
 		return nil, err
 	}
 	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
-	cls.Load("croncert.model")
+	if err := cls.Load("croncert.model"); err != nil {
+		return nil, err
+	}
+	labelsAttr := new(base.CategoricalAttribute)
+	attrBytes, err := os.ReadFile("croncert.class.json")
+	if err != nil {
+		return nil, err
+	}
+	if err := labelsAttr.UnmarshalJSON(attrBytes); err != nil {
+		return nil, err
+	}
+
 	ll := &Labler{
-		wordMap: w,
-		cls:     cls,
+		wordMap:    w,
+		cls:        cls,
+		labelsAttr: labelsAttr,
 	}
 	return ll, nil
 }
@@ -42,15 +55,20 @@ func (ll *Labler) PredictLabel(fValue ...string) string {
 		features = append(features, &f)
 	}
 	// https://github.com/sjwhitworth/golearn/blob/master/examples/instances/instances.go
-	attrs := make([]base.Attribute, 8)
-	for i := 0; i < 7; i++ {
+	attrs := make([]base.Attribute, len(FeatureList))
+	for i := 0; i < len(attrs)-1; i++ {
 		attrs[i] = base.NewFloatAttribute(FeatureList[i])
 	}
-	attrs[7] = new(base.CategoricalAttribute)
-	attrs[7].SetName(FeatureList[7])
-	for _, cl := range Classes {
-		attrs[7].GetSysValFromString(cl)
-	}
+	attrs[len(attrs)-1] = ll.labelsAttr
+	// attrs[len(attrs)-1] = new(base.CategoricalAttribute)
+	// catAttr, err := os.ReadFile("croncert.class.json")
+	// fmt.Println(err)
+	// err = attrs[len(attrs)-1].UnmarshalJSON(catAttr)
+	// fmt.Println(err)
+	// attrs[len(attrs)-1].SetName(FeatureList[len(attrs)-1])
+	// for _, cl := range Classes {
+	// 	attrs[len(attrs)-1].GetSysValFromString(cl)
+	// }
 
 	newInst := base.NewDenseInstances()
 	newSpecs := make([]base.AttributeSpec, len(attrs))
@@ -61,7 +79,7 @@ func (ll *Labler) PredictLabel(fValue ...string) string {
 
 	// fmt.Println(newInst.AllAttributes())
 
-	newInst.AddClassAttribute(newInst.AllAttributes()[7])
+	newInst.AddClassAttribute(newInst.AllAttributes()[len(attrs)-1])
 
 	newInst.Set(newSpecs[0], 0, newSpecs[0].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].digitCount)))
 	newInst.Set(newSpecs[1], 0, newSpecs[1].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].runeCount)))
@@ -70,25 +88,27 @@ func (ll *Labler) PredictLabel(fValue ...string) string {
 	newInst.Set(newSpecs[4], 0, newSpecs[4].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].colonCount)))
 	newInst.Set(newSpecs[5], 0, newSpecs[5].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].dashCount)))
 	newInst.Set(newSpecs[6], 0, newSpecs[6].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].dotCount)))
-	// newInst.Set(newSpecs[7], 0, newSpecs[7].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].class)))
+	newInst.Set(newSpecs[7], 0, newSpecs[7].GetAttribute().GetSysValFromString(fmt.Sprint(features[0].whitespaceCount)))
 	fmt.Println(newInst)
-	pred, _ := ll.cls.Predict(newInst)
-	// fmt.Println(err)
-	// fmt.Println(pred)
-	fmt.Printf("\n\nPrediction: %s\n", pred.RowString(0))
-	return pred.RowString(0)
+	pred, err := ll.cls.Predict(newInst)
+	fmt.Println(err)
+	fmt.Println(pred)
+	// fmt.Printf("\n\nPrediction: %s\n", pred.RowString(0))
+	// return pred.RowString(0)
+	return "title"
 }
 
 // Features contains all the relevant features and the class label
 type Features struct {
-	digitCount     int
-	runeCount      int
-	dictWordsCount int
-	slashCount     int
-	colonCount     int
-	dashCount      int
-	dotCount       int
-	class          string
+	digitCount      int
+	runeCount       int
+	dictWordsCount  int
+	slashCount      int
+	colonCount      int
+	dashCount       int
+	dotCount        int
+	whitespaceCount int
+	class           string
 }
 
 var FeatureList []string = []string{
@@ -99,6 +119,7 @@ var FeatureList []string = []string{
 	"colon-count",
 	"dash-count",
 	"dot-count",
+	"whitespace-count",
 	"class",
 }
 
@@ -118,14 +139,15 @@ var Classes []string = []string{
 
 func calculateFeatures(fName, fValue string, wordMap map[string]bool) Features {
 	return Features{
-		digitCount:     countDigits(fValue),
-		runeCount:      countRunes(fValue),
-		dictWordsCount: countDictWords(fValue, wordMap),
-		slashCount:     countRune(fValue, []rune("/")[0]),
-		colonCount:     countRune(fValue, []rune(":")[0]),
-		dashCount:      countRune(fValue, []rune("-")[0]),
-		dotCount:       countRune(fValue, []rune(".")[0]),
-		class:          fName,
+		digitCount:      countDigits(fValue),
+		runeCount:       countRunes(fValue),
+		dictWordsCount:  countDictWords(fValue, wordMap),
+		slashCount:      countRune(fValue, []rune("/")[0]),
+		colonCount:      countRune(fValue, []rune(":")[0]),
+		dashCount:       countRune(fValue, []rune("-")[0]),
+		dotCount:        countRune(fValue, []rune(".")[0]),
+		whitespaceCount: countRune(fValue, []rune(" ")[0]),
+		class:           fName,
 	}
 }
 
@@ -163,10 +185,10 @@ func writeFeaturesToFile(filename string, featuresChan <-chan *Features, wg *syn
 	}
 	defer featuresFile.Close()
 	writer := bufio.NewWriter(featuresFile)
-	writer.WriteString("digit-count, rune-count, dict-words-count, slash-count, colon-count, dash-count, dot-count, class\n")
+	writer.WriteString("digit-count, rune-count, dict-words-count, slash-count, colon-count, dash-count, dot-count, whitespace-count, class\n")
 	for f := range featuresChan {
 		// fmt.Println(f)
-		writer.WriteString(fmt.Sprintf("%d, %d, %d, %d, %d, %d, %d, %s\n",
+		writer.WriteString(fmt.Sprintf("%d, %d, %d, %d, %d, %d, %d, %d, %s\n",
 			f.digitCount,
 			f.runeCount,
 			f.dictWordsCount,
@@ -174,6 +196,7 @@ func writeFeaturesToFile(filename string, featuresChan <-chan *Features, wg *syn
 			f.colonCount,
 			f.dashCount,
 			f.dotCount,
+			f.whitespaceCount,
 			f.class))
 	}
 	writer.Flush()
@@ -253,6 +276,7 @@ func BuildModel(filename string) error {
 	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.75)
 	log.Println("training on trainData")
 	cls.Fit(trainData)
+	fmt.Println(cls.String())
 	predictions, err := cls.Predict(testData)
 	if err != nil {
 		return err
@@ -264,7 +288,17 @@ func BuildModel(filename string) error {
 	// fmt.Println(predictions)
 	fmt.Println(evaluation.GetSummary(confusionMat))
 	modelFName := "croncert.model"
+	classFName := "croncert.class.json"
 	log.Printf("storing model to file %s\n", modelFName)
+	jsonBytes, err := trainData.AllClassAttributes()[0].MarshalJSON()
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(classFName, jsonBytes, 0644); err != nil {
+		return err
+	}
+
+	// fmt.Println(string(jsonBytes))
 	return cls.Save(modelFName)
 }
 

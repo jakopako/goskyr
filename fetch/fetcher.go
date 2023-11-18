0ce4239ad5b4d3e19fb664b5076258e61bb38f60
jakopako <jakob.dhondt@web.de>
2023-11-18 16:37:26 +0100

    moved lower request context into Fetch method

--- fetch/fetcher.go
+++ fetch/fetcher.go
@@ -58,9 +58,9 @@ func (s *StaticFetcher) Fetch(url string, opts FetchOpts) (string, error) {
 type DynamicFetcher struct {
 	UserAgent        string
 	WaitMilliseconds int
-	ctx              context.Context
-	cancelParent     context.CancelFunc
-	cancel           context.CancelFunc
+	allocContext     context.Context
+	cancelAlloc      context.CancelFunc
+	// cancel           context.CancelFunc
 }
 
 func NewDynamicFetcher(ua string, ms int) *DynamicFetcher {
@@ -68,14 +68,12 @@ func NewDynamicFetcher(ua string, ms int) *DynamicFetcher {
 		chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.WindowSize(1920, 1080), // init with a desktop view (sometimes pages look different on mobile, eg buttons are missing)
 	)
-	parentCtx, cancelParent := chromedp.NewExecAllocator(context.Background(), opts...)
-	ctx, cancel := chromedp.NewContext(parentCtx)
+	allocContext, cancelAlloc := chromedp.NewExecAllocator(context.Background(), opts...)
 	d := &DynamicFetcher{
 		UserAgent:        ua,
 		WaitMilliseconds: ms,
-		ctx:              ctx,
-		cancelParent:     cancelParent,
-		cancel:           cancel,
+		allocContext:     allocContext,
+		cancelAlloc:      cancelAlloc,
 	}
 	if d.WaitMilliseconds == 0 {
 		d.WaitMilliseconds = 2000 // default
@@ -84,17 +82,19 @@ func NewDynamicFetcher(ua string, ms int) *DynamicFetcher {
 }
 
 func (d *DynamicFetcher) Cancel() {
-	d.cancelParent()
-	d.cancel()
+	d.cancelAlloc()
 }
 
 func (d *DynamicFetcher) Fetch(url string, opts FetchOpts) (string, error) {
+	// start := time.Now()
+	ctx, cancel := chromedp.NewContext(d.allocContext)
+	defer cancel()
 	// TODO: add user agent
 	var body string
 	sleepTime := time.Duration(d.WaitMilliseconds) * time.Millisecond
 	actions := []chromedp.Action{
 		chromedp.Navigate(url),
-		chromedp.Sleep(sleepTime), // for now
+		chromedp.Sleep(sleepTime),
 	}
 	delay := 500 * time.Millisecond // default is .5 seconds
 	if opts.Interaction.Delay > 0 {
@@ -132,8 +132,10 @@ func (d *DynamicFetcher) Fetch(url string, opts FetchOpts) (string, error) {
 	}))
 
 	// run task list
-	err := chromedp.Run(d.ctx,
+	err := chromedp.Run(ctx,
 		actions...,
 	)
+	// elapsed := time.Since(start)
+	// log.Printf("scraping %s took %s", url, elapsed)
 	return body, err
 }
